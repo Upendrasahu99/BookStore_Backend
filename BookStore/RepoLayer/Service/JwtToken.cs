5b0277e7850c664708159fe54557c691be9e89a9
Upendra Sahu <upendrasahu1199@gmail.com>
2023-11-08 14:38:29 +0530

    [Upendra] Add:Jwt Generation And Configuration Added

---
+++ BookStore/RepoLayer/Service/JwtToken.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RepoLayer.Service
+{
+    public class JwtToken
+    {
+        private readonly IConfiguration configuration;
+
+        public JwtToken(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string GenerateToken(string email, int userId, string role)
+        {
+            List<Claim> claimData = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, email),
+                new Claim("UserId", userId.ToString()),
+                new Claim("Role", role),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSetting:SecretKey"]));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer : "",
+                audience :"",
+                claims : claimData,
+                notBefore : DateTime.Now,
+                expires : DateTime.Now.AddMinutes(30),
+                signingCredentials : cred
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return 
+        }
+    }
+}
